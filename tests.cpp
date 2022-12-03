529b661754d5fa0b3708bcde0c156683d971e16c
turtlelazy <ishraqmahid@gmail.com>
2022-12-03 17:46:23 -0500

    task B tested

--- tests.cpp
+++ tests.cpp
@@ -21,3 +21,25 @@ TEST_CASE("TASK A")
     test_vector = makeVector(-5);
     CHECK(test_vector.size() == 0);
 }
+
+TEST_CASE("TASK B")
+{
+    std::vector<int> test_vector{1, 2, -1, 3, 4, -1, 6};
+    std::vector<int> compare_vector{1, 2, 3, 4, 6};
+    test_vector = goodVibes(test_vector);
+    CHECK(test_vector == compare_vector);
+
+    test_vector = {-1,-2,-3,-4};
+    compare_vector = {};
+    test_vector = goodVibes(test_vector);
+    CHECK(test_vector == compare_vector);
+
+    test_vector = {};
+    compare_vector = {};
+    test_vector = goodVibes(test_vector);
+    CHECK(test_vector == compare_vector);
+}
+
+TEST_CASE("TASK C"){
+    
+}
